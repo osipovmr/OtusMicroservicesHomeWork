e4090b6eb8c269cc10f369f04bdd6265f0152e3e
OsipovIlya <osipovmr@yandex.ru>
2023-09-15 22:00:06 +0300

    Response to json

--- src/main/java/otus/controller/Controller.java
+++ src/main/java/otus/controller/Controller.java
@@ -1,9 +1,9 @@
-package otus;
+package otus.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import otus.model.Response;
 
 @RestController
 public class Controller {
@@ -14,7 +14,7 @@ public class Controller {
     }
 
     @GetMapping("/health")
-    public ResponseEntity<Void> helloOtus() {
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<Response> helloOtus() {
+        return ResponseEntity.ok(Response.builder().status("OK").build());
     }
 }
