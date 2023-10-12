428ec31c308ba4f066f47bb11496caf36d3866df
OsipovIlya <osipovmr@yandex.ru>
2023-10-12 15:34:06 +0300

    Добавил базу данных

--- src/main/resources/db/changelog/changeset-insert.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
---changeset author:OsipovIlya
-
-insert into user_table (username, first_name, last_name, email, phone)
-values
-('osipovmr', 'ILYA', 'OSIPOV', 'osipovmr@yandex.ru', '+79112477069'),
-('otus', 'John', 'Spilberg', 'otus@gmail.com', '8(812)6009812'),
-('travel', 'Turkish', 'Airlines', 'fly@plane.sky', '00000');
